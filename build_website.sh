32c6db2ef4c409a1b01db46fd24bc5d6a195aae5
Søren Vinther Poulsen <soren.poulsen@polytechnique.edu>
2017-01-26 16:08:17 +0100

    Website update

--- build_website.sh
+++ build_website.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 
-git clone git@github.com:X-DataInitiative/mlpp.git tick
+TICK_DEFAULT_CLONE_FROM=git@github.com:X-DataInitiative/mlpp.git
+TICK_CLONE_FROM=${TICK_DEFAULT_CLONE_FROM}
 
-cur_dir=$(pwd)
+if [ $# -eq 1 ]
+  then
+    TICK_CLONE_FROM=$1
+fi
+
+echo "Cloning from ${TICK_CLONE_FROM}"
+git clone ${TICK_CLONE_FROM} tick
+
+CUR_DIR=$(pwd)
 
 (
   cd tick  && MLPP_NO_CPPLINT=true ./build_test.sh
 
-  export PYTHONPATH=${PYTHONPATH}:${cur_dir}/tick
+  TICK_BUILD_DIR=${CUR_DIR}/tick
+
+  TICK_LIBRARY_PATH=${TICK_BUILD_DIR}/mlpp/base/array/build:${TICK_BUILD_DIR}/mlpp/base/utils/build:${TICK_BUILD_DIR}/mlpp/base/math/build:${TICK_BUILD_DIR}/mlpp/random/build:${TICK_BUILD_DIR}/mlpp/optim/prox/build:${TICK_BUILD_DIR}/mlpp/optim/model/build
+
+  export PYTHONPATH=${PYTHONPATH}:${TICK_BUILD_DIR}
+  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${TICK_LIBRARY_PATH}
 
   cd DOC && make clean html
 )
@@ -18,4 +32,4 @@ git add -A
 git commit -am "Website update"
 git push
 
-#cd ${cur_dir}
+cd ${CUR_DIR}
