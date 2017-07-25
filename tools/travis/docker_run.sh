d8bda2ab71f509babf8ee45525b1ef8b13e4c78e
Søren Vinther Poulsen <soren.poulsen@polytechnique.edu>
2017-07-25 15:10:44 +0200

    TICK-391 Dirty hack

--- tools/travis/docker_run.sh
+++ tools/travis/docker_run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e -x
+set -ex
 
 # Copy tick source from host to container
 cp -R /io src
@@ -10,6 +10,17 @@ eval "$(pyenv init -)"
 
 pyenv global ${PYVER}
 
-python setup.py cpplint build_ext --inplace cpptest pytest
+python setup.py cpplint
+
+set +e
+python setup.py build_ext --inplace -j4
+python setup.py build_ext --inplace -j4 
+python setup.py build_ext --inplace -j4 
+python setup.py build_ext --inplace -j4 
+
+set -e
+
+python setup.py build_ext --inplace -j4 
+python setup.py cpptest pytest
 
 export PYTHONPATH=${PYTHONPATH}:`pwd` && (cd doc && make doctest)
