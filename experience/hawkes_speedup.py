b444a7a589fc178e3de3ed293e26a4a55b43f989
Martin B <mbompr@gmail.com>
2017-09-12 16:05:17 +0200

    TICK-406 add python speedup comparison

--- experience/hawkes_speedup.py
+++ experience/hawkes_speedup.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from tick.simulation import SimuHawkesExpKernels, SimuHawkesMulti
 from tick.inference import HawkesDual, HawkesExpKern
-from tick.plot import plot_history, plot_hawkes_kernel_norms
+import matplotlib.pyplot as plt
 
 np.random.seed(23928)
 
@@ -12,7 +12,7 @@ def estimation_error(estimated, original):
 
 simulate = False
 
-end_time = 10000
+end_time = 50000
 n_realizations = 5
 decay = 3.
 
@@ -41,8 +41,23 @@ else:
     timestamps = [timestamp for timestamp in timestamps]
 
 l_l2sq = 0.1
-hawkes_dual = HawkesDual(decay, l_l2sq, verbose=True, record_every=5,
-                         n_threads=4)
-hawkes_dual.fit(timestamps)
-#
-# plot_history([hawkes_dual], dist_min=True, log_scale=True)
+
+n_threads = range(1, 10)
+hawkes_duals = []
+
+for n_thread in n_threads:
+    hawkes_dual = HawkesDual(decay, l_l2sq, verbose=True, record_every=5,
+                             n_threads=n_thread)
+    hawkes_dual.fit(timestamps)
+    hawkes_duals += [hawkes_dual]
+
+time_one = hawkes_duals[0].history.last_values['time']
+plt.plot([n_threads[0], n_threads[-1]], [1, n_threads[-1]], ls='--',
+         lw=1, c='black')
+
+y = [time_one / hawkes_dual.history.last_values['time'] for hawkes_dual in hawkes_duals]
+plt.plot(n_threads, y)
+plt.xlabel('number of cores')
+plt.ylabel('speedup')
+
+plt.show()
