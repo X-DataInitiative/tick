18343b0b0ea578ca0d92e61b1a1b9d7d900b428e
peng <peng.wu.x2013@gmail.com>
2018-02-04 18:27:24 +0100

    SumExpLogLikList Ready!

--- tick/simulation/tests/test_8_sumexp_loss_list.py
+++ tick/simulation/tests/test_8_sumexp_loss_list.py
@@ -5,7 +5,6 @@ from tick.simulation import HawkesKernel0, HawkesKernelExp, HawkesKernelPowerLaw
     HawkesKernelSumExp
 from tick.simulation import SimuHawkes
 
-end_times = np.array([20.0, 20.0])
 timestamps_list = []
 global_n_list = []
 
@@ -15,7 +14,8 @@ seed = 3007
 MaxN_of_f = 10
 f_i = [np.array([1., 0.7, 0.8, 0.6, 0.5, 0.8, 0.3, 0.6, 0.2, 0.7]), np.array([1., 0.6, 0.8, 0.8, 0.6, 0.6, 0.5, 0.8, 0.3, 0.6])]
 
-end_time = 20.0
+end_time = 100.0
+end_times = np.array([end_time, end_time])
 betas = np.array([0.1, 1, 3, 10])
 
 U = len(betas)
@@ -33,10 +33,8 @@ simu_model.track_intensity(0.1)
 simu_model.simulate()
 
 timestamps = simu_model.timestamps
-timestamps.append(np.array([3.0]))
-
+timestamps.append(np.array([]))
 timestamps_list.append(timestamps)
-print(timestamps_list)
 
 global_n = np.array(simu_model._pp.get_global_n())
 global_n = np.insert(global_n, 0, 0).astype(int)
@@ -53,22 +51,34 @@ simu_model.track_intensity(0.1)
 simu_model.simulate()
 
 timestamps = simu_model.timestamps
-timestamps.append(np.array([4.0]))
-
+timestamps.append(np.array([]))
 timestamps_list.append(timestamps)
+
 global_n = np.array(simu_model._pp.get_global_n())
 global_n = np.insert(global_n, 0, 0).astype(int)
 global_n_list.append(global_n)
-
 
 ##################################################################################################################
 from tick.optim.model.hawkes_fixed_sumexpkern_loglik_custom_list import ModelHawkesFixedSumExpKernCustomLogLikList
 
 model_list = ModelHawkesFixedSumExpKernCustomLogLikList(betas, MaxN_of_f)
 model_list.fit(timestamps_list, global_n_list, end_times=end_times)
-
-print(timestamps_list[0])
 x0 = np.array(
     [0.6, 0.8,   0.2,0.2,0.2,0.2,  0.2,0.2,0.2,0.2,  0.4,0.4,0.4,0.4, 0.5,0.5,0.5,0.5,
      0.5, 0.5, 0.9, 0.9, 0.3, 0.6, 0.7, 0.8, 0.5,  0.5, 0.5, 0.9, 0.9, 0.3, 0.6, 0.7, 0.8, 0.5])
-print(model_list.loss(x0))
+
+
+tmp1 = 0
+tmp2 = 0
+from tick.optim.model import ModelHawkesSumExpCustom
+for i in range(2):
+    timestamps = timestamps_list[i]
+    global_n = global_n_list[i]
+    modelSumExp = ModelHawkesSumExpCustom(betas, MaxN_of_f)
+    modelSumExp.fit(timestamps, global_n, end_times[i])
+    tmp1 += modelSumExp.loss(x0) * (len(global_n) - 1)
+    tmp2 += (len(global_n) - 1)
+
+
+print("Loss calculated using list:", model_list.loss(x0))
+print("Loss calculated accurate  :", tmp1 / tmp2)
