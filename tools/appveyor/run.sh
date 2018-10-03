b2a6e9000c827759ce5101f16c3990edacce96b8
philip.deegan <philip.deegan@gmail.com>
2018-10-03 10:36:36 +0200

    drop gtests from appveyor - times out - investigate cmake

--- tools/appveyor/run.sh
+++ tools/appveyor/run.sh
@@ -21,13 +21,6 @@ rm -rf build/lib # force relinking of libraries in case of failure
 set -e
 /c/Python36-x64/python setup.py build_ext --inplace
 /c/Python36-x64/python -m unittest discover -v . "*_test.py"
-
-export MKN_CL_PREFERRED=1 # forces mkn to use cl even if gcc/clang are found
-# export MKN_COMPILE_THREADS=1 # mkn use 1 thread heap space issue
-export SWIG=0 # disables swig for mkn
-export CXXFLAGS="-EHsc"
-./sh/mkn.sh
-./sh/gtest.sh
 /c/Python36-x64/python setup.py bdist_wheel
 
 rm -rf build
