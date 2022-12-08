4cfb167dc8f14ee1126c7e0e7e1b2a8d7babb6ae
claudio <claudio.bellani01@gmail.com>
2022-12-08 11:18:39 +0000

    Examples - revert to no function flows

--- examples/plot_hawkes_varying_baseline.py
+++ examples/plot_hawkes_varying_baseline.py
@@ -13,48 +13,47 @@ piecewise constant.
 """
 
 import matplotlib.pyplot as plt
-
 from tick.plot import plot_hawkes_baseline_and_kernels, qq_plots
 from tick.hawkes import (SimuHawkesSumExpKernels, SimuHawkesMulti,
                          HawkesSumExpKern)
 
 
-def instantiate_and_run():
-    period_length = 300
-    baselines = [[0.3, 0.5, 0.6, 0.4, 0.2, 0], [0.8, 0.5, 0.2, 0.3, 0.3, 0.4]]
-    n_baselines = len(baselines[0])
-    decays = [.5, 2., 6.]
-    adjacency = [[[0, .1, .4], [.2, 0., .2]], [[0, 0, 0], [.6, .3, 0]]]
+################################################################################
+# instantiate and run
+################################################################################
+period_length = 300
+baselines = [[0.3, 0.5, 0.6, 0.4, 0.2, 0], [0.8, 0.5, 0.2, 0.3, 0.3, 0.4]]
+n_baselines = len(baselines[0])
+decays = [.5, 2., 6.]
+adjacency = [[[0, .1, .4], [.2, 0., .2]], [[0, 0, 0], [.6, .3, 0]]]
 
-    # simulation
-    hawkes = SimuHawkesSumExpKernels(baseline=baselines,
-                                     period_length=period_length,
-                                     decays=decays,
-                                     adjacency=adjacency,
-                                     seed=2093,
-                                     verbose=False,
-                                     )
-    hawkes.end_time = 1000
-    hawkes.adjust_spectral_radius(0.5)
+# simulation
+hawkes = SimuHawkesSumExpKernels(baseline=baselines,
+                                 period_length=period_length,
+                                 decays=decays,
+                                 adjacency=adjacency,
+                                 seed=2093,
+                                 verbose=False,
+                                 )
+hawkes.end_time = 1000
+hawkes.adjust_spectral_radius(0.5)
+multi = SimuHawkesMulti(hawkes, n_simulations=4)
+multi.simulate()
 
-    multi = SimuHawkesMulti(hawkes, n_simulations=4)
-    multi.simulate()
-
-    # estimation
-    learner = HawkesSumExpKern(decays=decays, n_baselines=n_baselines,
-                               period_length=period_length)
-
-    learner.fit(multi.timestamps)
-    return hawkes, learner
+# estimation
+learner = HawkesSumExpKern(decays=decays, n_baselines=n_baselines,
+                           period_length=period_length)
+learner.fit(multi.timestamps)
 
 
-def plot(learner, hawkes, show=True):
-    # plot
-    fig = plot_hawkes_baseline_and_kernels(learner, hawkes=hawkes, show=False)
-    if show:
-        fig.tight_layout()
-        plt.show()
-    return fig
+################################################################################
+# plot
+################################################################################
+show = True
+fig = plot_hawkes_baseline_and_kernels(learner, hawkes=hawkes, show=False)
+if show:
+    fig.tight_layout()
+    plt.show()
 
 
 def simulated_v_estimated_qq_plots(
@@ -88,13 +87,3 @@ def simulated_v_estimated_qq_plots(
         fig.tight_layout()
         plt.show()
     return fig
-
-
-def main():
-    hawkes, learner = instantiate_and_run()
-    fig = plot(learner, hawkes, show=True)
-    return fig
-
-
-if __name__ == '__main__':
-    main()
