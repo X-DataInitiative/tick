aaa57704b4680f9f9b29ff26999f3833f9656d49
claudio <claudio.bellani@transferwise.com>
2023-01-18 13:09:25 +0000

    debugging

--- lib/cpp-test/hawkes/inference/hawkes_em_gtest.cpp
+++ lib/cpp-test/hawkes/inference/hawkes_em_gtest.cpp
@@ -5,26 +5,57 @@
 
 class HawkesEMTest : public ::testing::Test {
  protected:
-  ulong n_nodes;
-  double kernel_support;
-  ulong kernel_size;
+  ulong n_nodes = 2;
+  double kernel_support = 1.;
+  ulong kernel_size = 10;
   ArrayDouble mu;
   ArrayDouble2d kernels;
-  HawkesEM em;
+  HawkesEM em{kernel_support, kernel_size, 1};
   void SetUp() override {
-    n_nodes = 2;
-    kernel_support = 1.;
-    kernel_size = 10;
-    em = HawkesEM(kernel_support, kernel_size, 1);
     mu = ArrayDouble{.05, .05};
-    kernels = ArrayDouble2d{
-        {.1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .2, .2, .1, .0, .0, .0, .0, .1, .2, .2},
-        {.0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .2, .2, .4, .1, .1, .0, .0, .0}};
+    kernels = ArrayDouble2d(n_nodes, n_nodes * kernel_size);
+    kernels[0] = .1;
+    kernels[1] = .1;
+    kernels[2] = .1;
+    kernels[3] = .1;
+    kernels[4] = .1;
+    kernels[5] = .1;
+    kernels[7] = .1;
+    kernels[8] = .1;
+    kernels[9] = .1;
+    kernels[10] = .2;
+    kernels[11] = .2;
+    kernels[12] = .1;
+    kernels[13] = .0;
+    kernels[14] = .0;
+    kernels[15] = .0;
+    kernels[17] = .1;
+    kernels[18] = .2;
+    kernels[19] = .2;
+    kernels[20] = .0;
+    kernels[21] = .0;
+    kernels[22] = .0;
+    kernels[23] = .0;
+    kernels[24] = .0;
+    kernels[25] = .0;
+    kernels[27] = .0;
+    kernels[28] = .0;
+    kernels[29] = .0;
+    kernels[30] = .2;
+    kernels[31] = .2;
+    kernels[32] = .4;
+    kernels[33] = .1;
+    kernels[34] = .1;
+    kernels[35] = .0;
+    kernels[37] = .0;
+    kernels[38] = .0;
+    kernels[39] = .0;
   }
 };
 
-TEST_F(HawkesEMTest, get_kernel_size) { EXPECT_EQ(em.get_kernel_size(), kernel_size); }
+TEST_F(HawkesEMTest, can_get_kernel_size) { EXPECT_EQ(em.get_kernel_size(), kernel_size); }
 
+/*
 TEST_F(HawkesEMTest, init_kernel_time_func) {
   em.init_kernel_time_func(kernels);
   std::vector<TimeFunction>& timefunc = em.get_kernel_time_func();
@@ -40,6 +71,7 @@ TEST_F(HawkesEMTest, init_kernel_time_func) {
     }
   }
 }
+*/
 
 #ifdef ADD_MAIN
 int main(int argc, char** argv) {
